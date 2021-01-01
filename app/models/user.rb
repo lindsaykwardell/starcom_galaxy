399889c212bb0211e0759fe2c9fff4857df6d768
Lindsay Wardell <three060@gmail.com>
2021-01-01 06:52:16 -0800

    Added basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
+
+  before_save :format_values
+
+  private
+
+  def format_values
+    self.email = email.downcase
+  end
 end
