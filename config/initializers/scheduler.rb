99f2d6c857f3ca003765485b49665a230a652dbc
Lindsay Wardell <three060@gmail.com>
2021-01-01 11:56:01 -0800

    Added TailwindCSS

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,11 +2,11 @@ require 'rufus-scheduler'
 
 s = Rufus::Scheduler.singleton
 
-s.every '30s' do
-  Empire.all.each do |empire|
-    empire.credits += 5
-    empire.industry += 2
-    empire.research += 2
-    empire.save
-  end
-end
+# s.every '30s' do
+#   Empire.all.each do |empire|
+#     empire.credits += 5
+#     empire.industry += 2
+#     empire.research += 2
+#     empire.save
+#   end
+# end
