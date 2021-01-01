1bdb2cf4930acf4dd2b342b3a6b5ee26decdf67a
Lindsay Wardell <three060@gmail.com>
2021-01-01 15:05:15 -0800

    Styling, structure

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
     require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
+    require("postcss-nested"),
     require("postcss-flexbugs-fixes"),
     require("postcss-preset-env")({
       autoprefixer: {
